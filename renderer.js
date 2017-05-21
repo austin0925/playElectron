79216e7fbbcb54b71e4b5a2281c1280b72176a5c
unknown <>
2017-05-21 14:02:15 -0700

    manage windows state is OK

--- renderer.js
+++ renderer.js
@@ -4,13 +4,36 @@
 const BrowserWindow = require('electron').remote.BrowserWindow
 const path = require('path')
 
+
+// first demo >> create a new window
 const newWindowBtn = document.getElementById('new-window')
 
 newWindowBtn.addEventListener('click', function (event) {
-  const modalPath = path.join('file://', __dirname, 'model.html')
+  const modalPath = path.join('file://', __dirname, 'modal.html')
   console.log('modalPath='+modalPath);
   let win = new BrowserWindow({ width: 1400, height: 320 })
   win.on('close', function () { win = null })
   win.loadURL(modalPath)
   win.show()
+})
+
+// second demo >> manage window
+const manageWindowBtn = document.getElementById('state-window');
+manageWindowBtn.addEventListener('click', (event) => {
+    const modalPath = path.join('file://', __dirname, 'modal.html')
+
+    let win = new BrowserWindow({width: 600, height: 1000});
+
+    win.on('resize', updateReply)
+    win.on('move', updateReply)
+    win.on('close', () => {win = null} )
+    win.loadURL(modalPath)
+    win.show()
+    function updateReply(){
+      const manageWindowsReply = document.getElementById('state-window-reply')
+      const message = `
+        Size: ${win.getSize()} Position: ${win.getPosition()}
+      `
+      manageWindowsReply.innerText = message
+    }
 })
